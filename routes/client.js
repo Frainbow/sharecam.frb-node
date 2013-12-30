e6405e8755c53d2bdfb7596c78c93a6a32c1d69a
martin <tlkuo.frb@gmail.com>
2013-12-30 15:36:52 +0800

    move the path of action

--- routes/client.js
+++ routes/client.js
@@ -5,7 +5,7 @@ exports.action = function (req, res) {
     var action = req.params.action;
 
     var device = net.connect({ host: '127.0.0.1', port: id }, function () {
-        device.write('GET /' + action + ' HTTP/1.1\r\n\r\n');
+        device.write('GET /client/' + action + ' HTTP/1.1\r\n\r\n');
     });
 
     device.on('data', function (data) {
